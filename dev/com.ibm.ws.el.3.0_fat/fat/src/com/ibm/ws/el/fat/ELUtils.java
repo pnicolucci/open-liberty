de08825d8b5511038375e66d73639a4599b25ee0
Paul Nicolucci <pnicolucci@gmail.com>
2020-03-02 23:49:43 -0500

    EL FAT only install app if not installed

---
+++ dev/com.ibm.ws.el.3.0_fat/fat/src/com/ibm/ws/el/fat/ELUtils.java
@@ -0,0 +1,37 @@
+/*******************************************************************************
+ * Copyright (c) 2020 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package com.ibm.ws.el.fat;
+
+import java.util.Set;
+import java.util.logging.Logger;
+
+import componenttest.topology.impl.LibertyServer;
+
+/**
+ * A utility class for EL tests.
+ */
+public class ELUtils {
+
+    protected static final Class<?> c = ELUtils.class;
+    private static final Logger LOG = Logger.getLogger(c.getName());
+
+    public static boolean isAppInstalled(LibertyServer libertyServer, String appName) throws Exception {
+        boolean appInstalled = false;
+        if (libertyServer.isStarted()) {
+            Set<String> installedApp = libertyServer.getInstalledAppNames(appName);
+            if (!installedApp.isEmpty()) {
+                appInstalled = true;
+            }
+        }
+        LOG.info("The following app: " + appName + " is installed: " + appInstalled);
+        return appInstalled;
+    }
+}
--- dev/com.ibm.ws.el.3.0_fat/fat/src/com/ibm/ws/el/fat/tests/EL30MethodExpressionInvocationsTest.java
+++ dev/com.ibm.ws.el.3.0_fat/fat/src/com/ibm/ws/el/fat/tests/EL30MethodExpressionInvocationsTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2018 IBM Corporation and others.
+ * Copyright (c) 2018, 2020 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -15,6 +15,7 @@ import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 
 import com.ibm.websphere.simplicity.ShrinkHelper;
+import com.ibm.ws.el.fat.ELUtils;
 import com.ibm.ws.el30.fat.servlets.EL30InvocationMethodExpressionsServlet;
 
 import componenttest.annotation.Server;
@@ -28,14 +29,17 @@ import componenttest.topology.utils.FATServletClient;
  */
 @RunWith(FATRunner.class)
 public class EL30MethodExpressionInvocationsTest extends FATServletClient {
+    private static final String APP_NAME = "TestEL3.0";
 
     @Server("elServer")
-    @TestServlet(servlet = EL30InvocationMethodExpressionsServlet.class, contextRoot = "TestEL3.0")
+    @TestServlet(servlet = EL30InvocationMethodExpressionsServlet.class, contextRoot = APP_NAME)
     public static LibertyServer elServer;
 
     @BeforeClass
     public static void setup() throws Exception {
-        ShrinkHelper.defaultDropinApp(elServer, "TestEL3.0.war", "com.ibm.ws.el30.fat.beans", "com.ibm.ws.el30.fat.servlets");
+        if (!ELUtils.isAppInstalled(elServer, APP_NAME)) {
+            ShrinkHelper.defaultDropinApp(elServer, APP_NAME + ".war", "com.ibm.ws.el30.fat.beans", "com.ibm.ws.el30.fat.servlets");
+        }
 
         elServer.startServer(EL30MethodExpressionInvocationsTest.class.getSimpleName() + ".log");
     }
