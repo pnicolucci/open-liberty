9335be7a5de3b2c3592a84750135f0719f39c1ab
mansal <mansal@us.ibm.com>
2025-03-13 14:15:21 -0400

    Avoid excessive flushing

--- dev/com.ibm.ws.transport.http/src/com/ibm/ws/http/netty/pipeline/HttpPipelineInitializer.java
+++ dev/com.ibm.ws.transport.http/src/com/ibm/ws/http/netty/pipeline/HttpPipelineInitializer.java
@@ -108,7 +108,6 @@ public class HttpPipelineInitializer extends ChannelInitializerWrapper {
     protected void initChannel(Channel channel) throws Exception {
         Tr.entry(tc, "initChannel");
         
-
         ChannelPipeline pipeline = channel.pipeline();
 
         // Initialize with the parent bootstrap initializer
