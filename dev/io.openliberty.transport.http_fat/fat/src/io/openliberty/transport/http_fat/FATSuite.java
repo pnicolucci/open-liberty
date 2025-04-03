6b3632d78775de656b21aaf55e4e479599a9001e
Paul Nicolucci <pnicolucci@gmail.com>
2025-04-03 12:48:58 -0400

    Add new FAT to test tcpOptions soLinger

--- dev/io.openliberty.transport.http_fat/fat/src/io/openliberty/transport/http_fat/FATSuite.java
+++ dev/io.openliberty.transport.http_fat/fat/src/io/openliberty/transport/http_fat/FATSuite.java
@@ -23,8 +23,11 @@ import io.openliberty.transport.http_fat.accesslists.AccessListsTests;
 @RunWith(Suite.class)
 @SuiteClasses({
                 AccessListsTests.class,
-                MaxOpenConnectionsTest.class
+                MaxOpenConnectionsTest.class,
+                PortOpenRetriesTests.class,
+                SoLingerTests.class
 })
+
 public class FATSuite {
 
     @ClassRule
