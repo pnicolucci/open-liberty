61ac090eec20c930fd59da9e6748ab3c0036d56f
Paul Nicolucci <pnicolucci@gmail.com>
2025-05-12 22:23:52 -0400

    Ensure AccessListHandler is sharable and not recreated for each connection

--- dev/io.openliberty.netty.internal.impl/src/io/openliberty/netty/internal/tcp/TCPChannelInitializerImpl.java
+++ dev/io.openliberty.netty.internal.impl/src/io/openliberty/netty/internal/tcp/TCPChannelInitializerImpl.java
@@ -37,11 +37,17 @@ public class TCPChannelInitializerImpl extends ChannelInitializerWrapper {
     TCPConfigurationImpl config;
 
 	NettyFrameworkImpl bundle;
+    AccessListHandler includeHandler;
     
     
     public TCPChannelInitializerImpl(BootstrapConfiguration config, NettyFrameworkImpl bundle) {
     	this.bundle = bundle;
         this.config = (TCPConfigurationImpl) config;
+
+       if(this.config.getAccessLists() != null) {
+           includeHandler = new AccessListHandler(this.config.getAccessLists());
+       }
+
     }
 
     @Override
@@ -62,7 +68,6 @@ public class TCPChannelInitializerImpl extends ChannelInitializerWrapper {
         }
         MaxOpenConnectionsHandler maxHandler = new MaxOpenConnectionsHandler(config.getMaxOpenConnections());
         if (config.getAccessLists() != null) {
-            AccessListHandler includeHandler = new AccessListHandler(config.getAccessLists());
             channel.pipeline().addLast(NettyConstants.ACCESSLIST_HANDLER_NAME, includeHandler);
         }
         channel.pipeline().addLast(NettyConstants.MAX_OPEN_CONNECTIONS_HANDLER_NAME, maxHandler);
